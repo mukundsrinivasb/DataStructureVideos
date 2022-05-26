71f101a78dea392317deec5887ae41329d388683
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2022-05-26 18:55:27 +1000

    Try to add a try delay after every visit of the node

--- DepthFirstSearch.cpp
+++ DepthFirstSearch.cpp
@@ -73,16 +73,26 @@ int main()
 	// Create a graph given in the above diagram
 	Graph g(9);
 	g.addEdge(0, 1);
+    g.addEdge(1, 0);
+    g.addEdge(0, 2);
+    g.addEdge(2, 0);
     g.addEdge(1, 4);
-    g.addEdge(4, 5);
+    g.addEdge(4, 1);
     g.addEdge(2, 3);
+    g.addEdge(3, 2);
     g.addEdge(3, 5);
+    g.addEdge(5, 3);
+    g.addEdge(4, 5);
+    g.addEdge(5, 4);
     g.addEdge(4, 6);
+    g.addEdge(6, 4);
     g.addEdge(4, 7);
+    g.addEdge(7, 4);
     g.addEdge(6, 8);
-
+    g.addEdge(8, 6);
+    
 	cout << "Following is Depth First Traversal"
-			" (starting from vertex 2) \n";
+			" (starting from vertex 4) \n";
 	g.DFS(4);
   cout << endl;
 	return 0;
